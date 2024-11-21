81bd6d43a0c59c25d4d97d936c0df0e3bd19ea04
Quentin Goinaud <quentin.goinaud@gmail.com>
2024-11-21 10:49:32 +0100

    update env

--- supabase/functions/_shared/supabase.ts
+++ supabase/functions/_shared/supabase.ts
@@ -3,7 +3,7 @@ import { Database } from './database.types.ts'
 
 const supabaseUrl = Deno.env.get('SUPABASE_URL')
 console.log('supabaseUrl', supabaseUrl)
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
+const supabaseAnonKey = Deno.env.get('SUPABASE_KEY')
 const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
 export {
