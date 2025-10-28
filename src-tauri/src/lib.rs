c9c879d5c3670adbd1bb13e0732bd7e0f984fe33
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-10-28 16:55:20 +0100

    -

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // .plugin(
+        //     tauri_plugin_log::Builder::new()
+        //         .level(tauri_plugin_log::log::LevelFilter::Debug)
+        //         .build(),
+        // )
         .plugin(tauri_plugin_http::init())
         .setup(|app| {
-            if cfg!(debug_assertions) {
+            // if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
+                        .level(log::LevelFilter::Debug)
                         .build(),
                 )?;
-            }
+            // }
             Ok(())
         })
         .run(tauri::generate_context!())
--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -3,18 +3,21 @@ import { Database } from '../../supabase/functions/_shared/database.types'
 import { fetch } from '@tauri-apps/plugin-http';
 import { isTauri } from '@/utils/tauri';
 
-console.log('meta', JSON.stringify(import.meta))
+import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
+
+
+info('meta:' + JSON.stringify(import.meta))
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
-console.log('supabaseUrl', supabaseUrl)
-console.log('isTauri', isTauri)
+info('supabaseUrl:' + supabaseUrl)
+info('isTauri:' + isTauri)
 
 const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
     global: {
         fetch: isTauri ? fetch : undefined,
-    }
+    },
 })
 
 export {
