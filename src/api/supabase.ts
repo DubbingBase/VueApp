dcebb393d54522a160f12b21cd0e69c0c8579b8a
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-11-04 14:10:38 +0100

    -

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,26 +1,23 @@
-import { createClient } from '@supabase/supabase-js'
-import { Database } from '../../supabase/functions/_shared/database.types'
-import { fetch } from '@tauri-apps/plugin-http';
-import { isTauri } from '@/utils/tauri';
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../supabase/functions/_shared/database.types";
+import { fetch } from "@tauri-apps/plugin-http";
+import { isTauri } from "@/utils/tauri";
 
-import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
+import { debug, error, info, trace, warn } from "@tauri-apps/plugin-log";
 
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (isTauri) {
-    info('meta:' + JSON.stringify(import.meta))
-    info('supabaseUrl:' + supabaseUrl)
-    info('isTauri:' + isTauri)
+    info("meta:" + JSON.stringify(import.meta));
+    info("supabaseUrl:" + supabaseUrl);
+    info("isTauri:" + isTauri);
 }
 
 const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
     global: {
         fetch: isTauri ? fetch : undefined,
     },
-})
+});
 
-export {
-    supabase
-}
+export { supabase };
